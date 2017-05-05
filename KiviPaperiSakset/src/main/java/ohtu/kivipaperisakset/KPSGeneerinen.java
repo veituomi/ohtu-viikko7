0982f03f822a53c2dfe79f97b504fdc0102b3717
Veini Tuominen <veini.tuominen@hotmail.com>
2017-05-06 00:08:33 +0300

    Vähennettiin toistoa Aiemmin havaittu mahdollisuus kilpailuttaa kahta tekoälyä ei toimi enää, joten pelaajalle 1 ei asetetakaan pelaajan 2 siirtoa.

--- KiviPaperiSakset/src/main/java/ohtu/kivipaperisakset/KPSGeneerinen.java
+++ KiviPaperiSakset/src/main/java/ohtu/kivipaperisakset/KPSGeneerinen.java
@@ -7,25 +7,21 @@ public abstract class KPSGeneerinen {
     protected void pelaa(Pelaaja p1, Pelaaja p2, String lause1, String lause2) {
         Tuomari tuomari = new Tuomari();
 
-        System.out.print(lause1);
-        String ekanSiirto = p1.annaSiirto();
-
-        System.out.print(lause2);
-        String tokanSiirto = p2.annaSiirto();
-
-        while (onkoOkSiirto(ekanSiirto) && onkoOkSiirto(tokanSiirto)) {
-            tuomari.kirjaaSiirto(ekanSiirto, tokanSiirto);
-            System.out.println(tuomari);
-            System.out.println();
-
+        while (true) {
             System.out.print(lause1);
-            ekanSiirto = p1.annaSiirto();
-            p1.asetaSiirto(tokanSiirto);
+            String ekanSiirto = p1.annaSiirto();
 
             System.out.print(lause2);
-            tokanSiirto = p2.annaSiirto();
+            String tokanSiirto = p2.annaSiirto();
             p2.asetaSiirto(ekanSiirto);
 
+            if (onkoOkSiirto(ekanSiirto) && onkoOkSiirto(tokanSiirto)) {
+                tuomari.kirjaaSiirto(ekanSiirto, tokanSiirto);
+                System.out.println(tuomari);
+                System.out.println();
+            } else {
+                break;
+            }
         }
 
         System.out.println();
