c5a41fd1297b4ca01f4c50743f307c015a5a2175
Veini Tuominen <veini.tuominen@hotmail.com>
2017-05-05 23:20:53 +0300

    Poistettiin turha tuonti

--- KiviPaperiSakset/src/main/java/ohtu/kivipaperisakset/KPSParempiTekoaly.java
+++ KiviPaperiSakset/src/main/java/ohtu/kivipaperisakset/KPSParempiTekoaly.java
@@ -2,8 +2,6 @@ package ohtu.kivipaperisakset;
 
 import java.util.Scanner;
 
-import java.util.Scanner;
-
 // Kivi-Paperi-Sakset, jossa voidaan valita pelataanko vastustajaa
 // vastaan vai ei
 public class KPSParempiTekoaly {
